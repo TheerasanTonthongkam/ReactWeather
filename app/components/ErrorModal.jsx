7f2b5108f0ad901d11781c66408e6b69fc9b67ad
Theerasan Tonthongkam <theerasan@kitchenup.com>
2017-02-15 16:17:59 +0700

    Add sass

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -8,7 +8,7 @@ var ErrorModal = React.createClass({
   },
   propTypes: {
     title: React.PropTypes.string,
-    message: React.PropTypes.string.isRequied
+    message: React.PropTypes.string.isRequired
   },
   componentDidMount: function () {
     var modal = new Foundation.Reveal($('#error-modal'));
