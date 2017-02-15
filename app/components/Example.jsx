5f7e12da8b1caa36eb70e3e1abfac39f473c1767
Theerasan Tonthongkam <theerasan@kitchenup.com>
2017-02-15 12:20:32 +0700

    add Example page and main style

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,9 +1,28 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Example = (props) => {
   return (
     <div>
-      <h3>Example</h3>
+      <h1 className="text-center">Example</h1>
+      <p>Some Example</p>
+      <ol>
+        <li>
+          <Link to='/?city=London'>
+            London
+          </Link>
+        </li>
+        <li>
+          <Link to='/?city=Bankgog'>
+            Bankgog
+          </Link>
+        </li>
+        <li>
+          <Link to='/?city=Hatyai'>
+            Hatyai
+          </Link>
+        </li>
+      </ol>
     </div>
   )
 };
